4453590f560e58a80726b37bc95c204a16c04382
xamfy <spratyaksh22@gmail.com>
2020-10-12 23:04:39 +0530

    Add Alternating Button and TextInputLayout

--- app/src/main/java/com/example/dynamicui/MainActivity.kt
+++ app/src/main/java/com/example/dynamicui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -26,28 +27,49 @@ class MainActivity : AppCompatActivity() {
 
         params.setMargins(40, 20, 40, 20)
 
-        for (i in 1..3) {
+        for (i in 1..4) {
 //            val editText = EditText(this)
-            val textInputLayout = TextInputLayout(
-                this,
-                null,
-                R.style.Widget_MaterialComponents_TextInputLayout_OutlinedBox
-            )
-            textInputLayout.layoutParams = params
 
-            textInputLayout.hint = "Enter your email address"
-            textInputLayout.boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
-            textInputLayout.setBoxCornerRadii(5F,
-                5F, 5F, 5F)
+            if(i%2 == 1) {
+                val textField = getTextField(params)
+                linearLayout.addView(textField)
+            } else {
+                val button = getButton(params)
+                linearLayout.addView(button)
+            }
 
-            val textInputEditText = TextInputEditText(textInputLayout.context)
-            textInputEditText.layoutParams = params
-
-            textInputLayout.addView(textInputEditText)
-
-            textInputLayout.layoutParams = params
-            linearLayout.addView(textInputLayout)
+//            linearLayout.addView(textInputLayout)
         }
 
+    }
+
+    private fun getTextField(params: LinearLayout.LayoutParams): TextInputLayout {
+        val textInputLayout = TextInputLayout(
+            this,
+            null,
+            R.style.Widget_MaterialComponents_TextInputLayout_OutlinedBox
+        )
+        textInputLayout.layoutParams = params
+
+        textInputLayout.hint = "Enter your email address"
+        textInputLayout.boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
+        textInputLayout.setBoxCornerRadii(5F,
+            5F, 5F, 5F)
+
+        val textInputEditText = TextInputEditText(textInputLayout.context)
+        textInputEditText.layoutParams = params
+
+        textInputLayout.addView(textInputEditText)
+
+        textInputLayout.layoutParams = params
+
+        return textInputLayout
+    }
+
+    private fun getButton(params: LinearLayout.LayoutParams): MaterialButton {
+        val button: MaterialButton = MaterialButton(this)
+        button.layoutParams = params
+        button.text = "Button"
+        return button
     }
 }
