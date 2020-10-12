42bd63af5843f77732afe478edd9c67da4fd7ffe
xamfy <spratyaksh22@gmail.com>
2020-10-12 23:18:46 +0530

    Add LinearLayoutParams to linearLayout

--- app/src/main/java/com/example/dynamicui/MainActivity.kt
+++ app/src/main/java/com/example/dynamicui/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.dynamicui
 
 import android.os.Bundle
-import android.widget.EditText
+import android.view.Gravity
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -19,22 +20,31 @@ class MainActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         val linearLayout: LinearLayout = findViewById(R.id.linearLayout)
+        val linearLayoutParams: ViewGroup.MarginLayoutParams =
+            linearLayout.layoutParams as ViewGroup.MarginLayoutParams
+        linearLayoutParams.topMargin = 50
 
         val params = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
 
+        val buttonParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        buttonParams.gravity = Gravity.CENTER
+
         params.setMargins(40, 20, 40, 20)
 
         for (i in 1..4) {
 //            val editText = EditText(this)
 
-            if(i%2 == 1) {
+            if (i % 2 == 1) {
                 val textField = getTextField(params)
                 linearLayout.addView(textField)
             } else {
-                val button = getButton(params)
+                val button = getButton(buttonParams)
                 linearLayout.addView(button)
             }
 
@@ -53,8 +63,10 @@ class MainActivity : AppCompatActivity() {
 
         textInputLayout.hint = "Enter your email address"
         textInputLayout.boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
-        textInputLayout.setBoxCornerRadii(5F,
-            5F, 5F, 5F)
+        textInputLayout.setBoxCornerRadii(
+            5F,
+            5F, 5F, 5F
+        )
 
         val textInputEditText = TextInputEditText(textInputLayout.context)
         textInputEditText.layoutParams = params
